82eac5dd279e4192deeab354aa526e30029f4f7c
Carlos C <Carlos C@DESKTOP-5IAH77Q.localdomain>
2021-03-17 12:14:35 +0100

    nuevo nuevisimo

--- src/main/java/com/mycompany/tienda/Opinion.java
+++ src/main/java/com/mycompany/tienda/Opinion.java
@@ -9,7 +9,7 @@ package com.mycompany.tienda;
  *
  * @author Carlos C
  */
-public class Opinion {
+public class Opinion implements Comparable{
     
     private String usuario;
     private Puntuacion puntuacion;
@@ -18,20 +18,47 @@ public class Opinion {
     private int votoNegativo;
     
     public Opinion(){
-        
+        votoPositivo = 0;
+        votoNegativo = 0;
     }
     public Opinion (String usuario, Puntuacion puntuacion, String comentario, int votoPositivo, int votoNegativo){
         
         this.usuario = usuario;
         this.puntuacion = puntuacion;
         this.comentario  = comentario;
-        this.votoPositivo = votoPositivo;
-        this.votoNegativo = votoNegativo;
+        votoPositivo = 0;
+        votoNegativo = 0;
+    }
+
+    public Puntuacion getPuntuacion() {
+        return puntuacion;
     }
     
-    public void DarVotoPositivo(int votoPositivo){
-        if(votoPositivo > 0){
-        }
+    //Methods
+    public void votoPositivo(){
+        this.votoPositivo++;
+    }
+    
+    public void votoNegativo(){
+        this.votoNegativo++;
+    }
+    
+    @Override
+    public String toString(){
+        StringBuilder sb = new StringBuilder();
+        sb.append("Usuario=").append(usuario);
+        sb.append("\npuntuacion=").append(puntuacion);
+        sb.append("\ncomentario=").append(comentario);
+        sb.append("\npositivos=").append(votoPositivo);
+        sb.append("\nnegativos=").append(votoNegativo);        
+        
+        return sb.toString();
+    }
+
+    @Override
+    public int compareTo(Opinion o) {
+        //Horrible, Fatal, Regular, Superable, Bien, Notable, Excelente
+        //this.Puntuacion = Puntuacion.BIEN --> mayor que REGULAR, MAL.
     }
     
 }
